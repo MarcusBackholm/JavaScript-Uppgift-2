3fe54ce711426faac3727a0d4cb7bbe68562ea41
Marcus Backholm <Marcus.Backholm@live.se>
2021-02-12 16:45:41 +0100

    changed classes to ID and minor changes to HTML code and CSS

---
+++ main.js
@@ -0,0 +1,17 @@
+let button = document.querySelector('mybutton');
+let searchbar = document.querySelector('searchbar');
+
+let cityname = document.querySelector('cityname')
+let day = document.querySelector('day')
+let temp = document.querySelector('temp')
+let condition = document.querySelector('conditions')
+
+button.addEventListener('click', function(){
+
+    fetch()
+    .then(Response=>Response.json)
+    .then(data=> console.log(data))
+    .catch(err=> alert("City not found!"))
+
+})
+
