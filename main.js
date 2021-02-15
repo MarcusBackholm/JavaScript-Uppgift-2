4c38d4edb6cf00238ba67f6c78b306fb89457027
Marcus Backholm <Marcus.Backholm@live.se>
2021-02-15 12:35:01 +0100

    Started with Javascript button search with API. Also added keys in an own document.txt

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-let button = document.querySelector('mybutton');
-let searchbar = document.querySelector('searchbar');
+// Declaring variables to ID elements
 
-let day = document.querySelector('day')
-let cityname = document.querySelector('cityname')
-let day = document.querySelector('day')
-let temp = document.querySelector('temp')
-let condition = document.querySelector('conditions')
+let button = document.querySelector('buttonSearch'); // The button.
+let searchbar = document.querySelector('searchbar'); // The searchbar.
+let day = document.querySelector('day') // What day it is.
+let cityname = document.querySelector('cityname') // The name of the city.
+let temperature = document.querySelector('temp') // The remperature.
+let condition = document.querySelector('conditions') // The weather conditions.
+let adress = document.querySelector('adress') // The specified adress.
+let description = document.querySelector('description') // Information at each attraction place.
 
+// Creating event calls with button.
+
+
+
+//make the button get the API value when searching on citys with given information from API.
 button.addEventListener('click', function(){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q= '+textInput.value+' &appid=d9af8ba6b94987343a5e32bf943c0cd0')
+    .then(Response => Response.json())
+    .then(data => {
+        let nameValue = data['city'];
+        let tempValue = data['main']['temp'];
+        let description = data['weather'][0]['description'];
 
-    fetch()
-    .then(Response=>Response.json)
-    .then(data=> console.log(data))
-    .catch(err=> alert("City not found!"))
+        city.innerHTML =nameValue;
+        temp.innerHTML = tempValue;
+        description.innerHTML = descriptionValue
+    })
 
+    .catch(Error => alert('Wrong city name!'))
 })
-
