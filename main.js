ee1c4cd858fd710aeefdad70b4e06d53ba86b3d7
Marcus Backholm <Marcus.Backholm@live.se>
2021-02-15 22:22:18 +0100

    fixed weather API

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// input           ID = inputText         TYPE = text        PLACEHOLDER = Search for destinaton
+// input           ID = search            TYPE = text        PLACEHOLDER = Search for destinaton
 // button          ID = button            TYPE = submit
 
 // checkbox 1      ID = onlyWeather       TYPE = Checkbox    NAME = travel-information1 
@@ -22,16 +22,36 @@
 // APIkey: d9af8ba6b94987343a5e32bf943c0cd0
 // URL: https://api.openweathermap.org/data/2.5/weather
 
+// Creating query for button action. 
+const button = document.querySelector('#button'); // Refering to button ID
 
-// Foursquare API
-const clientId = 'X5MAVFZN1F5NGMFX1LZ0P2MDFYQEQHPLT0R503XHOJMLEFGY';
-const clientSecretKey = 'CXO3WOL1BKKHSHCOE3N4XE3T5DRMVFQOW3BVHU5APWCKTC1Q';
-const URL  = 'https://api.foursquare.com/v2/venues/explore?near';
+button.addEventListener('click', event => {
+    WeatherBalloon();
+}) 
 
-//OpenWeather API
-const openWeatherApi = 'd9af8ba6b94987343a5e32bf943c0cd0'
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-var button = document.querySelector('.button')
-var inputText = document.querySelector('input')
-var name = document.querySelector('.name');
+// # för ID . för class
+const inputSearch = document.querySelector('#search') // Refering to Searchbar ID
+
+// Getting weather API information. 
+function WeatherBalloon( search ) {
+    var key = '{X5MAVFZN1F5NGMFX1LZ0P2MDFYQEQHPLT0R503XHOJMLEFGY}';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputSearch.value +'&appid=d9af8ba6b94987343a5e32bf943c0cd0')
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+        const city = data.name; // konstant för sökvärde 
+        const temp = data.main.temp; // konstant för temperatur
+        const weath = data.weather[0].description; // konstant för vädret.
+        console.log(data); // Loggar datorn vid körning.
+
+        let cityName = document.getElementById('cityName')
+        let temperature = document.getElementById('temperature')
+        let weather = document.getElementById('weather')
+        cityName.innerHTML = city;
+        temperature.innerHTML = temp;
+        weather.innerHTML = weath;
+    })
+    .catch(function() {
+        //Catch any errors
+    });
+}
