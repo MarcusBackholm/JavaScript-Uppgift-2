a6ecd6b6334a564b3fde1d42bc3c53370cea1aea
Marcus Backholm <Marcus.Backholm@live.se>
2021-04-12 23:42:04 +0200

    Klar, får ej alfabetisk ordning att fungera.

--- main.js
+++ main.js
@@ -95,23 +95,6 @@ document.getElementById("c2").onchange = function(e){
     let sec = document.getElementById("box1");
     sec.classList.toggle("removed");
 }
-function sortSelect(selElem) {
-    var tmpAry = new Array();
-    for (var i=0;i<selElem.options.length;i++) {
-        tmpAry[i] = new Array();
-        tmpAry[i][0] = selElem.options[i].text;
-        tmpAry[i][1] = selElem.options[i].value;
-    }
-    tmpAry.sort();
-    while (selElem.options.length > 0) {
-        selElem.options[0] = null;
-    }
-    for (var i=0;i<tmpAry.length;i++) {
-        var op = new Option(tmpAry[i][0], tmpAry[i][1]);
-        selElem.options[i] = op;
-    }
-    return;
-}
 //Rensar sökhistorik för attractions.
 resetSearch.addEventListener("click", () => {
     let e = document.querySelector("#box2");     
